68a373549b2d009109072e12fb4a2b2d876bc0dd
“luoyang-123” <2488851381@qq.com>
2021-05-06 15:13:51 +0800

    first commit

--- app.py
+++ app.py
@@ -3,24 +3,23 @@ from utils import base62_decode, base62_encode
 from db import get_buohao_index, add_url, get_long_url
 
 app = Flask(__name__)
-q=0
-index=0
+toekn,index=0,0
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    global q,index
+    global toekn,index
     if request.method == 'GET':
         return render_template('index.html')
     else:
         url = request.form['url']
-        if q==0:
+        if toekn==0:
             index = get_buohao_index()
-            q=index-100
-        if q==index:
+            toekn=index-100
+        if toekn==index:
             index = get_buohao_index()
             print(index)
-        q+=1
-        token = base62_encode(q)
+        toekn+=1
+        token = base62_encode(toekn)
         add_url(url, token)
         short_url = 'http://127.0.0.1:8002/{token}'.format(token=token)
         return jsonify({'short_url': short_url})
